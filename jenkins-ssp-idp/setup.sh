cb649dae523957026a13d2c398e28ab73ae20788
Mahendran Mookkiah <mahendran.mookkiah@gmail.com>
2022-05-24 18:57:50 -0400

    jenkins-ssp-idp cosmetic changes

--- jenkins-ssp-idp/setup.sh
+++ jenkins-ssp-idp/setup.sh
@@ -6,19 +6,16 @@ cd `git rev-parse --show-toplevel`/jenkins-ssp-idp
 
 # To compare the real changes and adopt future helm chart changes... 
 # helm show values jenkins/jenkins > values-as-is.yaml 
-export RELEASE_NAME=jenkins-local
+export RELEASE_NAME=jenkins
 
 # One time and do not commit the password
-# kubectl create secret generic jenkins-credentials --namespace  jenkins-ssp-idp \
-#   --from-literal=jenkins-admin-user=admin \
-#   --from-literal=jenkins-admin-password=YourFavoritePassword
+kubectl create secret generic jenkins-credentials --namespace  jenkins-ssp-idp \
+  --from-literal=jenkins-admin-user=admin \
+  --from-literal=jenkins-admin-password=YourFavoritePassword
+sudo bash -c 'echo "127.0.0.1    jenkins-saml-sp.mm-local.com" >> /etc/hosts'
 
-# use the above image in values.yaml
+
 helm --namespace jenkins-ssp-idp uninstall $RELEASE_NAME
 helm --namespace jenkins-ssp-idp upgrade -i $RELEASE_NAME jenkins/jenkins  -f values.yaml --debug
-
-kubectl exec --namespace jenkins-ssp-idp -it svc/$RELEASE_NAME -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
-
-sudo bash -c 'echo "127.0.0.1    jenkins-saml-sp.mm-local.com" >> /etc/hosts'
 
 open https://jenkins-saml-sp.mm-local.com
