8842415ed925cb35897dc3d195e2d95be708c257
Mahendran Mookkiah <mahendran.mookkiah@gmail.com>
2022-06-12 19:18:59 -0400

    SP cert added

--- ssp-sp-gluu-idp/setup.sh
+++ ssp-sp-gluu-idp/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-kubectl create namespace ssp-sp-gluu-idp 
-kubectl config set-context --current --namespace ssp-sp-gluu-idp
+export NAMESPACE=ssp-sp-gluu-idp
+env | grep 'NAMESPACE'
+
+kubectl create namespace $NAMESPACE --ignore-not-found
+kubectl config set-context --current --namespace $NAMESPACE
 cd `git rev-parse --show-toplevel`/ssp-sp-gluu-idp
+
+
 ##### One Time or Validation commands are commented
 # We need SSL certificate for SP to encrypt/sign based on the need
 #mkdir -p cert
@@ -10,20 +15,23 @@ cd `git rev-parse --show-toplevel`/ssp-sp-gluu-idp
 #openssl x509 -in cert/server.crt -text -noout
 #sudo bash -c 'echo "127.0.0.1    ssp-sp-gluu-idp.mm-local.com" >> /etc/hosts'
 
-kubectl -n ssp-sp-gluu-idp delete secret x509keycert --ignore-not-found
-kubectl -n ssp-sp-gluu-idp create secret generic x509keycert --from-file=cert/server.pem --from-file=cert/server.crt
+kubectl -n $NAMESPACE delete secret x509keycert --ignore-not-found
+kubectl -n $NAMESPACE create secret generic x509keycert --from-file=cert/server.pem --from-file=cert/server.crt
 
-kubectl -n ssp-sp-gluu-idp  delete configmap config.php --ignore-not-found
-kubectl -n ssp-sp-gluu-idp  create configmap config.php --from-file=config/config.php
+kubectl -n $NAMESPACE  delete configmap config.php --ignore-not-found
+kubectl -n $NAMESPACE  create configmap config.php --from-file=config/config.php
 
-kubectl -n ssp-sp-gluu-idp  delete configmap saml20-idp-remote.php --ignore-not-found
-kubectl -n ssp-sp-gluu-idp  create configmap saml20-idp-remote.php --from-file=metadata/saml20-idp-remote.php
+kubectl -n $NAMESPACE delete configmap authsources.php --ignore-not-found
+kubectl -n $NAMESPACE create configmap authsources.php --from-file=config/authsources.php
 
-kubectl -n ssp-sp-gluu-idp  delete configmap httpdconf --ignore-not-found
-kubectl -n ssp-sp-gluu-idp  create configmap httpdconf --from-file=httpd-conf/httpd.conf
+kubectl -n $NAMESPACE  delete configmap saml20-idp-remote.php --ignore-not-found
+kubectl -n $NAMESPACE  create configmap saml20-idp-remote.php --from-file=metadata/saml20-idp-remote.php
 
-kubectl -n ssp-sp-gluu-idp  apply -f deployment.yaml 
-kubectl -n ssp-sp-gluu-idp  apply -f service.yaml
-kubectl -n ssp-sp-gluu-idp  apply -f ingress.yaml    
+kubectl -n $NAMESPACE  delete configmap httpdconf --ignore-not-found
+kubectl -n $NAMESPACE  create configmap httpdconf --from-file=httpd-conf/httpd.conf
+
+kubectl -n $NAMESPACE  apply -f deployment.yaml 
+kubectl -n $NAMESPACE  apply -f service.yaml
+kubectl -n $NAMESPACE  apply -f ingress.yaml    
 
 open https://ssp-sp-gluu-idp.mm-local.com/simplesaml/
