d9c90d13667305c4947c6ae38e5fb2a0d8638c69
Mahendran Mookkiah <mahendran.mookkiah@gmail.com>
2022-06-19 12:00:59 -0400

    cosmetic changes

--- jenkins-ssp-idp/setup.sh
+++ jenkins-ssp-idp/setup.sh
@@ -4,7 +4,7 @@ export NAMESPACE=jenkins-ssp-idp
 export RELEASE_NAME=jenkins
 env | grep 'NAMESPACE\|RELEASE_NAME'
 
-kubectl create namespace $NAMESPACE --ignore-not-found
+kubectl create namespace $NAMESPACE
 kubectl config set-context --current --namespace $NAMESPACE
 cd `git rev-parse --show-toplevel`/jenkins-ssp-idp
 
@@ -18,7 +18,9 @@ kubectl create secret generic jenkins-credentials --namespace  $NAMESPACE \
   --from-literal=jenkins-admin-password=YourFavoritePassword
 sudo bash -c 'echo "127.0.0.1    jenkins-saml-sp.example.com" >> /etc/hosts'
 
+# Reinstalling helm will create new SP Metadata
 helm --namespace $NAMESPACE uninstall $RELEASE_NAME
+
 helm --namespace $NAMESPACE upgrade -i $RELEASE_NAME jenkins/jenkins  -f values.yaml --debug
 
 # SP Metadata
